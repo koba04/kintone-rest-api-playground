8c3b873de392b42af5b37720a5313b3045585019
Toru Kobayashi <koba0004@gmail.com>
2019-10-14 14:34:22 +0900

    fix: an umd build

--- packages/kintone-api-client/rollup.config.js
+++ packages/kintone-api-client/rollup.config.js
@@ -1,4 +1,7 @@
 import typescript from "rollup-plugin-typescript";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
 
 export default {
   input: "./src/index.ts",
@@ -7,5 +10,5 @@ export default {
     format: "umd",
     name: "KintoneApiClient"
   },
-  plugins: [typescript()]
+  plugins: [commonjs(), resolve({ browser: true }), json(), typescript()]
 };
